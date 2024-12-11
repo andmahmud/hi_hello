624e322f4b4b056bdc4a1166d5262a3912fecc4c
MD.MAHMUDUL HASAN <126802456+andmahmud@users.noreply.github.com>
2024-12-11 16:51:41 +0600

    update

--- lib/cards.dart
+++ lib/cards.dart
@@ -1,166 +1,199 @@
 import 'package:flutter/material.dart';
 
-class CardsPage extends StatelessWidget {
+class CardsPage extends StatefulWidget {
+  @override
+  _CardsPageState createState() => _CardsPageState();
+}
+
+class _CardsPageState extends State<CardsPage> {
+  int _currentIndex = 0; // To track the current card index
+
+  final List<Color> _buttonColors = [
+    Colors.purple,
+    const Color.fromARGB(255, 88, 23, 118),
+  ]; // Colors for each card's SHARE button
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          // Wrap everything in a Column widget
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Card(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(16),
-              ),
-              elevation: 4,
-              child: Container(
-                width: 300,
-                height: 530,
-                child: Column(
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            child: PageView.builder(
+              controller: PageController(viewportFraction: 0.85),
+              onPageChanged: (index) {
+                setState(() {
+                  _currentIndex = index; // Update the current index
+                });
+              },
+              itemCount: 2,
+              itemBuilder: (context, index) {
+                return Column(
                   mainAxisSize: MainAxisSize.min,
                   children: [
-                    Stack(
-                      children: [
-                        ClipRRect(
-                          borderRadius: const BorderRadius.vertical(
-                            top: Radius.circular(16),
-                          ),
-                          child: Image.asset(
-                            'assets/images/image.png',
-                            height: 250, // Increased height for the image
-                            width: double.infinity,
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                        Positioned(
-                          top: 10,
-                          left: 0,
-                          right: 0,
-                          child: Center(
-                            child: Container(
-                              padding: const EdgeInsets.symmetric(
-                                vertical: 4,
-                                horizontal: 8,
-                              ),
-                              decoration: BoxDecoration(
-                                color: Colors.grey,
-                                borderRadius: BorderRadius.circular(8),
-                              ),
-                              child: const Text(
-                                "NEW",
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold,
+                    Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                      elevation: 4,
+                      child: Container(
+                        width: double.infinity,
+                        height: 550,
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Stack(
+                              children: [
+                                ClipRRect(
+                                  borderRadius: const BorderRadius.vertical(
+                                    top: Radius.circular(16),
+                                  ),
+                                  child: Image.asset(
+                                    index == 0
+                                        ? 'assets/images/image.png'
+                                        : 'assets/images/image.png',
+                                    height: 200,
+                                    width: double.infinity,
+                                    fit: BoxFit.cover,
+                                  ),
                                 ),
+                                Positioned(
+                                  top: 10,
+                                  left: 0,
+                                  right: 0,
+                                  child: Center(
+                                    child: Container(
+                                      padding: const EdgeInsets.symmetric(
+                                        vertical: 4,
+                                        horizontal: 8,
+                                      ),
+                                      decoration: BoxDecoration(
+                                        color: Colors.grey,
+                                        borderRadius: BorderRadius.circular(8),
+                                      ),
+                                      child: Text(
+                                        index == 0 ? "NEW" : "WOARK",
+                                        style: const TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    index == 0 ? "MD MAHMUD HASAN" : "MAHMUD",
+                                    style: const TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  const SizedBox(height: 4),
+                                  Text(
+                                    index == 0
+                                        ? "Software Engineer"
+                                        : "UI/UX Designer",
+                                    style: TextStyle(
+                                      fontSize: 14,
+                                      color: Colors.grey[700],
+                                    ),
+                                  ),
+                                  Text(
+                                    index == 0 ? "Developer" : "Freelancer",
+                                    style: TextStyle(
+                                      fontSize: 14,
+                                      color: index == 0
+                                          ? Colors.blue
+                                          : Colors.green,
+                                    ),
+                                  ),
+                                  Text(
+                                    index == 0 ? "Skz" : "Creative Studio",
+                                    style: TextStyle(
+                                      fontSize: 14,
+                                      color: Colors.grey[700],
+                                    ),
+                                  ),
+                                  const SizedBox(height: 16),
+                                  Row(
+                                    children: [
+                                      const Icon(Icons.email,
+                                          color: Colors.grey),
+                                      const SizedBox(width: 8),
+                                      Text(index == 0
+                                          ? "skillerszonetanvir@gmail.com"
+                                          : "jane.doe@example.com"),
+                                    ],
+                                  ),
+                                  const SizedBox(height: 8),
+                                  Row(
+                                    children: [
+                                      Icon(
+                                        index == 0
+                                            ? Icons.facebook
+                                            : Icons.facebook,
+                                        color: Colors.grey,
+                                      ),
+                                      const SizedBox(width: 8),
+                                      Text(
+                                          index == 0 ? "Facebook" : "LinkedIn"),
+                                    ],
+                                  ),
+                                  const SizedBox(height: 8),
+                                  Row(
+                                    children: [
+                                      const Icon(Icons.phone_android,
+                                          color: Colors.grey),
+                                      const SizedBox(width: 8),
+                                      Text(
+                                          index == 0 ? "7650247" : "123456789"),
+                                    ],
+                                  ),
+                                ],
                               ),
                             ),
-                          ),
+                          ],
                         ),
-                      ],
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const Text(
-                            "MD MAHMUD HASAN",
-                            style: TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          const SizedBox(height: 4),
-                          Text(
-                            "Software Engineer",
-                            style: TextStyle(
-                              fontSize: 16,
-                              color: Colors.grey[700],
-                            ),
-                          ),
-                          const Text(
-                            "Developer",
-                            style: TextStyle(
-                              fontSize: 16,
-                              color: Colors.blue,
-                            ),
-                          ),
-                          Text(
-                            "Skz",
-                            style: TextStyle(
-                              fontSize: 16,
-                              color: Colors.grey[700],
-                            ),
-                          ),
-                          const SizedBox(height: 16),
-                          const Row(
-                            children: [
-                              Icon(Icons.email, color: Colors.grey),
-                              SizedBox(width: 8),
-                              Text("skillerszonetanvir@gmail.com"),
-                            ],
-                          ),
-                          const SizedBox(height: 8),
-                          const Row(
-                            children: [
-                              Icon(Icons.facebook, color: Colors.grey),
-                              SizedBox(width: 8),
-                              Text("Facebook"),
-                            ],
-                          ),
-                          const SizedBox(height: 8),
-                          const Row(
-                            children: [
-                              Icon(Icons.phone_android, color: Colors.grey),
-                              SizedBox(width: 8),
-                              Text("7650247"),
-                            ],
-                          ),
-                        ],
                       ),
                     ),
                   ],
+                );
+              },
+            ),
+          ),
+          // Constant SHARE button
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                foregroundColor: Colors.white,
+                backgroundColor: _buttonColors[_currentIndex], // Dynamic color
+                padding: const EdgeInsets.symmetric(
+                    vertical: 8.0, horizontal: 50.0), // Updated padding
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
                 ),
+                elevation: 5,
+                minimumSize:
+                    Size(double.infinity, 50), // Make button full width
+              ),
+              onPressed: () {
+                print('Share button pressed for card $_currentIndex');
+              },
+              child: const Text(
+                'SHARE',
+                style: TextStyle(color: Colors.white, fontSize: 18),
               ),
             ),
-            const SizedBox(height: 20), // Space between the card and the button
-            // Share button placed below the card
-            Container(
-              width: double
-                  .infinity, // To ensure it stretches across the available width
-              padding: const EdgeInsets.symmetric(horizontal: 11.0),
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  foregroundColor: Colors.white,
-                  backgroundColor: Colors.purple, // Set text color to white
-                  padding: const EdgeInsets.symmetric(
-                      vertical: 8.0), // Padding inside the button
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(
-                        15), // Rounded corners with 10% radius
-                  ),
-                  elevation: 5, // Add shadow to the button for a 3D effect
-                ),
-                onPressed: () {
-                  print('Share button pressed');
-                  // Handle share action here
-                },
-                child: const Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    SizedBox(width: 5),
-                    Text(
-                      'SHARE',
-                      style: TextStyle(color: Colors.white, fontSize: 15),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ],
-        ),
+          )
+        ],
       ),
     );
   }
