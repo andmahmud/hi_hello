d83d5c8d9f5bb56b8ee090834350261923e5e62d
MD.MAHMUDUL HASAN <126802456+andmahmud@users.noreply.github.com>
2024-12-21 18:11:41 +0600

    update

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:HiHello/get_started_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
